c3f8625de20063d0439990a19b188046b624a955
Daniel <dsouzaborini@gmail.com>
2021-02-24 18:17:12 -0300

    add parte 4

---
+++ bloco_4/dia_1/Parte 4/scripts.js
@@ -0,0 +1,9 @@
+let notaDaniel = 90;
+
+if (notaDaniel > 80) {
+    console.log("Parabéns, você foi aprovada(o)!")
+} else if (notaDaniel < 80 && notaDaniel > 60) {
+    console.log("Você está na nossa lista de espera")
+} else {
+    console.log("Você foi reprovado(a)")
+}
