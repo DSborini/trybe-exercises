ce692b4cd07c934aaed0fc9ce940e337f77c0dd0
Daniel <dsouzaborini@gmail.com>
2021-02-24 16:08:36 -0300

    add parte 3

--- bloco_4/dia_1/scripts.js
+++ bloco_4/dia_1/scripts.js
@@ -1,7 +1,13 @@
-const name = "Daniel";
-const birthCity = "Guararapes"
-const birthYear = 2002;
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
 
-console.log(name);
-console.log(birthCity);
-console.log(birthYear);
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);
